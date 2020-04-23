0f40d1ce83f54c4efeb1daed7fc42e87644a74d0
Jyotsna <jagjyotsna@gmail.com>
2020-04-24 01:30:23 +0530

    SumOfParentOfX

--- someOfNodes.py
+++ someOfNodes.py
@@ -6,10 +6,10 @@ class Node:
 
 node8 = Node(8)
 node12 = Node(12)
-node16 = Node(16)
+node16 = Node(8)
 node25 = Node(25)
 node10 = Node(10, node8, node12)
-node20 = Node(10, node16, node25)
+node20 = Node(20, node16, node25)
 node15 = Node(15, node10, node20)
 
 def sum_of_nodes(head, result=0):
@@ -41,6 +41,24 @@ def SumOfChildOfX(head, target, result=0):
         result += SumOfChildOfX(head.left, target)
     
     return result
+    
+    
+def SumOfParentOfX(head, target, result=0):
+    
+    if head is not None:    
+        if head.right is not None and head.right.val == target:
+            result += head.val
+        elif head.left is not None and head.left.val == target:
+            result += head.val
+        
+    if head.right is not None:
+        result += SumOfParentOfX(head.right, target)
+        
+    if head.left is not None:
+        result += SumOfParentOfX(head.left, target)
+    
+    return result
         
 #print(sum_of_nodes(node15))
-print(SumOfChildOfX(node15, 10))
+#print(SumOfChildOfX(node15, 10))
+print(SumOfParentOfX(node15, 8))
